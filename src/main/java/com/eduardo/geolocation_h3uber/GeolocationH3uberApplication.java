9b3f17629f5ec68cdf112fbd7a2aa232846455ac
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2026-02-18 14:24:59 -0300

    first commit

---
+++ src/main/java/com/eduardo/geolocation_h3uber/GeolocationH3uberApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.geolocation_h3uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationH3uberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeolocationH3uberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eduardo/geolocation_h3uber/GeolocationH3uberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.geolocation_h3uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationH3uberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
